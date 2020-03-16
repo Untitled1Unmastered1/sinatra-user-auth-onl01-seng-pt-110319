7b792bc29b61a270a204db7647d942f8206d85dd
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-03-16 04:08:35 +0000

    Automatically backed up by Learn

--- db/migrate/create_users_01.rb
+++ db/migrate/create_users_01.rb
@@ -1,6 +1,6 @@
 Class CreateUsers < ActiveRecord::Migration[5.1]
   def change 
-    create table :article do |t|
+    create table :articles do |t|
       t.string :name 
       t.string :email 
       t.string :password 
